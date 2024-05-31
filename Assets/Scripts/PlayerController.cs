a08a0805a876a194c259d796897b1db44de3c568
Jorge Medina <jorgemedinaleigh@protonmail.com>
2024-05-31 18:55:26 -0400

    Interaction
    -Added a crosshair to the HUD
    -Implemented simple interaction to turn off lights

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private CharacterController CharacterController;
+
+    [Header("Player Stats")]
     [SerializeField] private float walkSpeed = 1f;
+    [SerializeField] private float playerReach = 2f;
+
+    private Interactable currentInteractable;
 
     private void Update()
     {
         HandleWalking();
+        CheckInteraction();
+        InteractWithObject();
     }
 
     private void HandleWalking()
@@ -20,5 +27,60 @@ public class PlayerController : MonoBehaviour
         Vector3 movementDirection = transform.right * x + transform.forward * z;
 
         CharacterController.Move(movementDirection * walkSpeed * Time.deltaTime);
+    }
+
+    private void CheckInteraction()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+
+        if(Physics.Raycast(ray, out hit, playerReach))
+        {
+            if(hit.collider.tag == "Interactable")
+            {
+                Interactable newInteractable = hit.collider.GetComponent<Interactable>();
+
+                if(newInteractable.enabled)
+                {
+                    SetNewCurrentInteractable(newInteractable);
+                }
+                else
+                {
+                    DisableCurrentInteractable();
+                }
+            }
+            else
+            {
+                DisableCurrentInteractable();
+            }
+        }
+        else
+        {
+            DisableCurrentInteractable();
+        }
+    }
+
+    private void SetNewCurrentInteractable(Interactable newInteractable)
+    {
+        currentInteractable = newInteractable;
+        HUDController.instance.EnableInteractionText(currentInteractable.message);
+    }
+
+    private void DisableCurrentInteractable()
+    {
+        HUDController.instance.DisableInteractionText();
+
+        if(currentInteractable)
+        {
+            currentInteractable = null;
+        }
+    }
+
+    private void InteractWithObject()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && currentInteractable != null)
+        {
+            currentInteractable.Interact();
+        }
     }
 }
